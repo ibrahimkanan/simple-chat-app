a571db33e6729ad3c86f5a5e1fa2e434fe5f47ae
ibrahimkanan <ibrahimkanan20@gmail.com>
2026-07-15 17:41:54 +0300

    fix : use the right url for socket io

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("http://localhost:5003", {
+            const socket = io("https://simple-chat-app-ro03.onrender.com", {
                 query: {
                     userId: authUser._id,
                 },
